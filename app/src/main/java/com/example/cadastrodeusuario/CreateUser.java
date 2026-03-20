f61abb1b16b0a4c8e9901fe068c1a3df0cf3c221
Cohen <arielecohendepaula@gmail.com>
2026-03-19 21:30:13 -0300

    Comite de conexão ao repositório

---
+++ app/src/main/java/com/example/cadastrodeusuario/CreateUser.java
@@ -0,0 +1,17 @@
+package com.example.cadastrodeusuario;
+
+//Bloco de Importações
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+//Classe principal
+public class CreateUser extends AppCompatActivity {
+    TextInputEditText editNome, editEmail;
+
+    Button btnSalvar;
+
+
+}
---
+++ app/src/main/java/com/example/cadastrodeusuario/UserAdapter.java
@@ -0,0 +1,4 @@
+package com.example.cadastrodeusuario;
+
+public class UserAdapter {
+}
