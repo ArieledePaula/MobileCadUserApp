1f7fa4843a3b35d6fba401b832755659a88e2454
Cohen <arielecohendepaula@gmail.com>
2026-03-19 21:51:20 -0300

    Adicionando Create User

--- app/src/main/java/com/example/cadastrodeusuario/CreateUser.java
+++ app/src/main/java/com/example/cadastrodeusuario/CreateUser.java
@@ -1,6 +1,7 @@
 package com.example.cadastrodeusuario;
 
 //Bloco de Importações
+import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,24 @@ public class CreateUser extends AppCompatActivity {
 
     Button btnSalvar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
 
+        // Vincula variável com elemente de interface gráfica, escrito no arquivo xml
+        editNome = findViewById(R.id.nome);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener(v -> {
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if(!nome.isEmpty()){
+                MainActivity.listaNomes.add(nome + "(" + email + ") ");
+                finish();
+            }
+        });
+    }
 }
