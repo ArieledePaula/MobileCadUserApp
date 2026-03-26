17ff9a684bb3c91d1ef235109de206c415190bdc
Cohen <arielecohendepaula@gmail.com>
2026-03-26 20:24:04 -0300

    Criando método 3 - Informando ao Android quantos itens a lista possui ao todo e fazendo mapeamento do ID do layout

--- app/src/main/java/com/example/cadastrodeusuario/UserAdapter.java
+++ app/src/main/java/com/example/cadastrodeusuario/UserAdapter.java
@@ -3,9 +3,10 @@ package com.example.cadastrodeusuario;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
@@ -20,19 +21,21 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     }
 
     //Método 1: Cria "do zero" o visual de uma linha da lista (+ViewHolder)
+
     @NontNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //LayoutInflater transforma o arquivo XMLem um objeto View Java
         //Aqui usamos um layout padrão do Android (simple_list_item1) para facilitar
         View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
 
         //Retorna uma nova instância da nossa classe interna ViewHolder com a View Criada
         return new ViewHolder(view);
+    }
 
-        //Método 2: Vincula os dados de um objeto de lista a uma linha específica da tela
 
-        @Override
+    //Método 2: Vincula os dados de um objeto de lista a uma linha específica da tela
+    @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position){
             //Recupera os dados da lista de acordo com a posição que o Android está desenhado agora
             String nome = listaUsuarios.get(position);
@@ -41,5 +44,25 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             holder.tvNome.setText(nome);
         }
 
-    }
+    //Método 3:Informa ao Android quantos itens a lista possui ao todo
+    @Override
+        public int getItemCount(){
+        //Se a lista existir, retorna o tamanho. Se não, retorna zero
+
+            return listaUsuarios != null ? listaUsuarios.size() : 0;
+        }
+
+        //Classe internaViewHolder: Servir para "segurar" as referências dos componentes de cada linha
+        //Evitar chamadas respectivas ao findViewByID, melhorando a performance do RecycleView
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            //Referência parao TextView da Linha
+            TextView tvNome;
+
+            public ViewHolder(@NonNull View itemView){
+                super(itemView);
+                //Faz o mapeamento do ID do layout para o objeto java
+                //android.R.id.text1 é o ID padrão do layout 'simple_list_item_1'
+                tvNome = itemView.findViewById(android.R.id.text1);
+            }
+        }
 }
